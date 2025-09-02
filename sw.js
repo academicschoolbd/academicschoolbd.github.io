4df214c064158d7ed042c3267ac632fb0b32bc77
academicschoolbd <159193937+academicschoolbd@users.noreply.github.com>
2025-09-02 12:46:30 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,5 @@
-const CACHE_NAME = 'bengali-app-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-// Install: Cache core assets
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-// Fetch: Serve from cache if offline
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });
